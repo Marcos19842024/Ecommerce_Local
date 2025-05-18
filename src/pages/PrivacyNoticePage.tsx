a6e850e4b71fe974e506271591b27d76eecae6f2
Marcos19842024 <chable240608@hotmail.com>
2025-05-17 22:44:13 -0600

    v38

--- src/pages/PrivacyNoticePage.tsx
+++ src/pages/PrivacyNoticePage.tsx
@@ -5,106 +5,106 @@ export const PrivacyNoticePage = () => {
             <h1 className='text-3xl font-bold text-center my-4'>
                 Aviso de privacidad
             </h1>
-            <div className="containerterm">
-                <p className="term4">
+            <div className="p-5">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
                         Última actualización:
                     </strong> 15 de mayo de 2025
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         EL PROVEEDOR del presente aviso manifiesta ser el propietario de este establecimiento con domicilio y denominaci&oacute;n descrita en este documento, se&ntilde;ala como domicilios para o&iacute;r y recibir notificaciones en el domicilio antes citado, as&iacute; como manifiesta ser EL RESPONSABLE DEL USO, PROTECCI&Oacute;N y MANEJO DEL TRATAMIENTO DE SUS DATOS PERSONALES, al respecto le informo que sus datos personales que recabamos de Usted referente a su EDAD, NOMBRE COMPLETO, SU CLAVE DEL RFC Y CURP, DOMICILIO FISCAL Y PERSONAL ACTUAL COMPLETO, C&Oacute;DIGO POSTAL, TEL&Eacute;FONO DE CASA, TRABAJO, CELULAR, CORREO ELECTR&Oacute;NICO, &nbsp;FACEBOOK, E INSTAGRAM, as&iacute; como &nbsp;LOS CONTENIDOS EN EL MATERIAL FOTOGR&Aacute;FICO, AUDIO Y VIDEO, DIAGRAMAS, GR&Aacute;FICOS, DIBUJOS O REPRESENTACIONES GR&Aacute;FICAS QUE TOMAMOS EN NUESTRAS INSTALACIONES DE LAS IM&Aacute;GENES de las personas y animales que ingresan y transitan por nuestras instalaciones, ser&aacute;n protegidos de conformidad a lo dispuesto por la Ley General de Protecci&oacute;n de Datos Personales en Posesi&oacute;n de Sujetos Obligados, as&iacute; como todos aquellos ordenamientos que resulten aplicables, y los utilizaremos para uno o m&aacute;s de los siguientes conceptos:
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         1.- Complementar los datos del propietario o poseedor en los contratos, convenios y formatos que usamos para prestar nuestros servicios que le brindamos y en el expediente de nuestros servicios para su animal.
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         2.- Expedir sus notas y facturas por nuestros servicios.&nbsp;
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         3.- Para localizarlo de forma oportuna ante alguna emergencia que requiera de su presencia y autorizaci&oacute;n para la realizar estudio, aprobaci&oacute;n de gastos e insumos para atender a su animal, uso de anestesia e intervenciones quir&uacute;rgicas en su ser vivo antes citado en caso de ser necesario para salvaguardar su vida.
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         &nbsp;4.- Informarle de forma oportuna el estado que guarda su orden de servicio que nos solicit&oacute;.
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         &nbsp;5.- Para usar el MATERIAL FOTOGR&Aacute;FICO, AUDIO Y VIDEO en la VIDEO SEGURIDAD de nuestro establecimiento, as&iacute; como EN LA UTILIZACI&Oacute;N DE NUESTRA PUBLICIDAD por cualquier medio respecto a la prestaci&oacute;n de nuestros servicios hacia Usted y su animal.&nbsp;
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         6.- Cualquier finalidad an&aacute;loga o compatible con las anteriores.
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         Le manifestamos que la informaci&oacute;n de sus datos personales que nos proporcione es necesaria para el servicio solicitado, la cual nos permite brindarle una mejor atenci&oacute;n y seguridad, por lo que NO requerimos m&aacute;s datos personales adicionales de Usted. La negativa para el uso de sus datos personales para estas finalidades o la negativa a usar el MATERIAL FOTOGR&Aacute;FICO, AUDIO Y VIDEO en la VIDEO SEGURIDAD de nuestro establecimiento, as&iacute; como EN LA UTILIZACI&Oacute;N DE NUESTRA PUBLICIDAD, NO es motivo para negarle nuestros los servicios y productos que solicita o contrata con nosotros, salvo las requeridas por la ley al respecto. La autorizaci&oacute;n para utilizar del MATERIAL FOTOGR&Aacute;FICO, AUDIO Y VIDEO en la VIDEO SEGURIDAD de nuestro establecimiento, as&iacute; como EN LA UTILIZACI&Oacute;N DE NUESTRA PUBLICIDAD por cualquier medio respecto a la prestaci&oacute;n de nuestros servicios hacia Usted y su animal, estar&aacute; vigente hasta en tanto no presente por escrito solicitud de negativa a la captaci&oacute;n, difusi&oacute;n y utilizaci&oacute;n de la imagen y voz antes referidas, la cual deber&aacute; ser presentada en nuestro domicilio. Para conocer m&aacute;s informaci&oacute;n de los t&eacute;rminos y condiciones en que ser&aacute;n tratados sus datos personales, como los terceros con quienes compartimos su informaci&oacute;n personal y la forma en que podr&aacute; ejercer sus derechos ARCO, puede consultarnos en nuestro domicilio referido al final del presente aviso. Por todo lo antes explicado significa que ha le&iacute;do, entendido y est&aacute; de acuerdo con los t&eacute;rminos antes expuestos, por lo que otorgo mi consentimiento para que mis datos personales sean tratados conforme a lo se&ntilde;alado en el presente aviso de privacidad y consiento la transmisi&oacute;n de mis datos personales en t&eacute;rminos del presente aviso. Otorgando adem&aacute;s al escrito de este aviso, autorizaci&oacute;n expresa para el Uso y difusi&oacute;n &nbsp;promoci&oacute;n y publicidad de sus servicios en diversos medios de comunicaci&oacute;n de mi Imagen y mi animal, para que sean utilizadas y difundidas, indefinidamente, sin restricci&oacute;n, ni contraprestaci&oacute;n de ning&uacute;n tipo, a t&iacute;tulo gratuito a favor del suscrito de este aviso, por cualquier medio impreso, f&iacute;lmico e internet, tendiente a la difusi&oacute;n de nuestras actividades, programas y campa&ntilde;as, ya que la autorizaci&oacute;n antes citada, no me causa da&ntilde;os ni perjuicios morales, ni econ&oacute;micos, con la aparici&oacute;n y utilizaci&oacute;n de mi nombre o la de mi animal, nuestra imagen en video y/o fotograf&iacute;a y/o voz de mi persona o la de mi animal ya que &nbsp;NO se lesiona en ning&uacute;n momento nuestra imagen, fama, reputaci&oacute;n o integridad.
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         Al leer y no ejercer su posici&oacute;n por escrito a lo antes citado, bien entrar a nuestras instalaciones es sin&oacute;nimo de haber aceptado a lo antes expresado y lo consideraremos como aceptado el presente Aviso de Privacidad y otorgada su autorizaci&oacute;n para todos los efectos se&ntilde;alados.
                     </span>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         De igual forma le informamos que no damos consentimiento alguno para que usted grabe de forma alguna ni voz, ni video en nuestras instalaciones de persona alguna o animales en atenci&oacute;n a los derechos de personalidad de las personas y sus animales que se encuentren en nuestras instalaciones. &nbsp; &nbsp;
                     </span>
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
-                        <span className="term5">
+                        <span className='text-base leading-normal mb-2'>
                             ATENTAMENTE.
                         </span>
                     </strong>
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
-                        <span className="term5">
+                        <span className='text-base leading-normal mb-2'>
                             EL PROVEEDOR Propietario del Establecimiento.
                         </span>
                     </strong>
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
-                        <span className="term5">
+                        <span className='text-base leading-normal mb-2'>
                             M.V.Z. Francisco Juli&aacute;n G&oacute;mez Cancino.
                         </span>
                     </strong>
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
-                        <span className="term5">
+                        <span className='text-base leading-normal mb-2'>
                             Domicilio:
                         </span>
                     </strong>
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
-                        <span className="term5">
+                        <span className='text-base leading-normal mb-2'>
                             Animalia-Baja California No. 402 Col. Petrolera, C.P. 96500. Coatzacoalcos, Ver.
                         </span>
                     </strong>
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
-                        <span className="term5">
+                        <span className='text-base leading-normal mb-2'>
                             Baalak- Av. Central No.33 Col Santa Ana, C.P. 24050. San Fco. De Campeche, Campeche.
                         </span>
                     </strong>
                 </p>
-                <p className="term4">
-                    <span className="term5">
+                <p className='text-base leading-loose text-gray-800'>
+                    <span className='text-base leading-normal mb-2'>
                         *El presente aviso se expide y publica con fundamento en los Art&iacute;culos 6o. y 16 de la Constituci&oacute;n Pol&iacute;tica de los Estados Unidos Mexicanos, Art&iacute;culo 12 de la Declaraci&oacute;n Universal de los Derechos Humanos, Art&iacute;culo 17 del Pacto Internacional de Derechos Civiles y Pol&iacute;ticos, Art&iacute;culo 11 de la Convenci&oacute;n Americana sobre Derechos Humanos &ldquo;Pacto de San Jos&eacute;&rdquo;, Art&iacute;culos 68 y 69 de la Ley General de Transparencia y Acceso a la Informaci&oacute;n P&uacute;blica. 16, 113 y 117 de la Ley Federal de Transparencia y Acceso a la Informaci&oacute;n P&uacute;blica, Art&iacute;culos 3, fracciones II, III, X, XXXVII, XXXIII, 7, 18, 19, 20, 21, 22, 23, 26, 27, 28, 43, 47, 65 y dem&aacute;s de la Ley General de Protecci&oacute;n de Datos Personales en Posesi&oacute;n de Sujetos Obligados, y dem&aacute;s disposiciones legales aplicables vigentes y dem&aacute;s art&iacute;culos del C&oacute;digo Civil de la Entidad, as&iacute; como en t&eacute;rminos del art&iacute;culo 87 de la Ley Federal del Derecho de Autor.
                     </span>
                 </p>                
--- src/pages/TermsPage.tsx
+++ src/pages/TermsPage.tsx
@@ -4,82 +4,82 @@ export const TermsPage = () => {
             <h1 className='text-3xl font-bold text-center my-4'>
                 Términos y condiciones
             </h1>
-            <div className="containerterm">
-                <p className="term4">
+            <div className="p-5">
+                <p className='text-base leading-loose text-gray-800'>
                     <strong>
                         Última actualización:
                     </strong> 15 de mayo de 2025
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     {'Bienvenido a '}
                     <strong>
                         Baalak' Shop
                     </strong>. Al acceder y utilizar nuestro sitio web, aceptas los siguientes Términos y Condiciones. Te recomendamos leerlos cuidadosamente antes de utilizar nuestros servicios.
                 </p>
-                <h2 className="term4">
+                <h2 className='text-2xl mt-5'>
                     1. Información General
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Baalak' Shop es una tienda en línea ubicada en Campeche, México, dedicada a la venta de alimentos y productos relacionados para perros y gatos. Al utilizar nuestro sitio, aceptas cumplir con estos términos conforme a las leyes aplicables en México.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     2. Cuentas de Usuario
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Para realizar compras o acceder a ciertos servicios, puedes crear una cuenta. Es tu responsabilidad mantener la confidencialidad de tu información de acceso. No compartas tu contraseña con terceros.
                 </p>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Nos reservamos el derecho de suspender o eliminar cuentas que infrinjan estos términos, o que sean utilizadas de manera fraudulenta.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     3. Productos y Descripciones
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Hacemos todo lo posible por mostrar descripciones, imágenes y precios precisos. Sin embargo, no garantizamos que toda la información esté libre de errores. Nos reservamos el derecho de modificar o descontinuar productos sin previo aviso.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     4. Pagos
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Actualmente, Baalak' Shop no cobra suscripciones ni pagos recurrentes. Los precios están expresados en pesos mexicanos e incluyen impuestos, a menos que se indique lo contrario.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     5. Envíos y Entregas
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Ofrecemos entregas dentro de Campeche. Los tiempos de envío y tarifas se informarán al momento de la compra. No nos hacemos responsables por retrasos causados por terceros (paqueterías, clima, etc.).
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     6. Devoluciones y Cancelaciones
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Por tratarse de productos alimenticios para mascotas, no se aceptan devoluciones por cambio de opinión. Solo se aceptarán devoluciones si el producto está dañado, defectuoso o mal empacado. Para solicitar una devolución, contáctanos dentro de las primeras 48 horas después de la entrega.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     7. Uso del Sitio
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     El usuario se compromete a utilizar este sitio únicamente con fines lícitos y de manera que no infrinja los derechos de terceros ni las leyes mexicanas.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     8. Propiedad Intelectual
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Todo el contenido de este sitio (textos, imágenes, logotipos, etc.) es propiedad de Baalak' Shop o se utiliza con permiso. Queda prohibido su uso sin autorización previa por escrito.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     9. Modificaciones
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Nos reservamos el derecho de modificar estos Términos y Condiciones en cualquier momento. Cualquier cambio será publicado en esta página con su fecha de entrada en vigor.
                 </p>
-                <h2 className="term2">
+                <h2 className='text-2xl mt-5'>
                     10. Contacto
                 </h2>
-                <p className="term4">
+                <p className='text-base leading-loose text-gray-800'>
                     Si tienes dudas sobre estos Términos y Condiciones, puedes contactarnos en:
                 </p>
-                <ul className="term3">
+                <ul className='list-disc ml-5'>
                     <li>
                         <strong>
                             Correo electrónico:
