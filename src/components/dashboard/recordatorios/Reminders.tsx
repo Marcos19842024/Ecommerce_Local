60fb20506b3a86eebc1f50fe59e8ebb9df4e0156
marcos19842024 <chable240608@gmail.com>
2025-06-16 23:40:38 -0600

    16625

--- src/components/dashboard/recordatorios/Reminders.tsx
+++ src/components/dashboard/recordatorios/Reminders.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Cliente } from "../../../interfaces";
 import { toast } from "react-hot-toast/headless";
+import { VscSend } from "react-icons/vsc";
+import { PiPaperclipBold } from "react-icons/pi";
 
 interface Props {
 	clientes: Cliente[];
@@ -23,9 +25,9 @@ export const Reminders = ({ clientes }: Props) => {
         let msj
 
         if (imo) {
-        msj = `Hola ${clientes[index].nombre}.\n\nLa clínica veterinaria Baalak', le informa que ${clientes[index].mensaje}\n\n${msjo}`;
+            msj = `Hola ${clientes[index].nombre}.\n\nLa clínica veterinaria Baalak', le informa que ${clientes[index].mensaje}\n\n${msjo}`;
         } else {
-        msj = `Hola ${clientes[index].nombre}.\n\nLa clínica veterinaria Baalak', le informa que ${clientes[index].mensaje}`;
+            msj = `Hola ${clientes[index].nombre}.\n\nLa clínica veterinaria Baalak', le informa que ${clientes[index].mensaje}`;
         }
         return msj
     }
@@ -37,70 +39,82 @@ export const Reminders = ({ clientes }: Props) => {
     }
 
     return (
-        <div className='flex flex-col items-center h-full'>
-            <h1 className='text-2xl font-bold mb-2'>Recordatorios</h1>
+        <div className='flex flex-col gap-6 relative'>
             {clientes.length > 0 ?
-                <>
-                    <form
-                        className='flex flex-col gap-4 w-full max-w-md'
-                        onSubmit={handleSubmit}>
-                        <div className={`bg-white border border-gray-300 shadow-sm rounded-md flex flex-col gap-2 p-5 h-fit lg:col-span-2 lg:row-span-2`}>
-                            <h2 className='font-bold tracking-tight text-xl'>Clientes</h2>
-                                <select
-                                    className="w-full p-2 border border-gray-600 rounded-md"
-                                    onChange={(e) => {
-                                        setIndex(e.target.selectedIndex);
-                                    }}> {
-                                    clientes && clientes.map(item => (
-                                        <option
-                                            key={item.nombre}
-                                            value={item.nombre}> {item.nombre}
-                                        </option>
-                                    ))
-                                }
-                            </select>
-                            <p>Telefono: {clientes[index].telefono}</p>
-                            <p>Mascotas: {clientes[index].mascotas.map(mascota => (
-                                <span key={mascota.nombre}>{mascota.nombre}</span>
-                            ))}</p>
-                        </div>
-                        <div className={`bg-white border border-gray-300 shadow-sm rounded-md flex flex-col gap-2 p-5 h-fit lg:col-span-2 lg:row-span-2`}>
-                            <h2 className='font-bold tracking-tight text-xl'>Mensaje</h2>
-                            <textarea
-                                className="w-full p-2 border border-gray-600 rounded-md"
-                                value={sendMesage()}
-                                readOnly
-                                onChange={adjustHeight}>
-                            </textarea>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    onChange={(e) => {
-                                        setImo(e.target.checked)
-                                    }}
-                                />{'  Incluir mensaje opcional'}
-                            </label>
+                <form
+                    className='grid grid-cols-1 lg:grid-cols-2 gap-2 auto-rows-max flex-1'
+                    onSubmit={handleSubmit}>
+                    <div className='bg-white border border-gray-300 shadow-sm rounded-md flex flex-col gap-2 p-5 h-fit lg:row-span-2'>
+                        <h2 className='font-bold tracking-tight text-xl'>Clientes</h2>
+                            <select
+                                className='w-full p-2 border border-gray-600 rounded-md'
+                                onChange={(e) => {
+                                    setIndex(e.target.selectedIndex);
+                                }}> {
+                                clientes && clientes.map(item => (
+                                    <option
+                                        key={item.nombre}
+                                        value={item.nombre}> {item.nombre}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                        <p>Telefono: {clientes[index].telefono}</p>
+                        <p>Mascotas: {clientes[index].mascotas.map(mascota => (
+                            <span key={mascota.nombre}>{mascota.nombre}</span>
+                        ))}</p>
+                    </div>
+                    <div className='bg-white border shadow-sm rounded-md flex flex-col gap-2 p-5 h-fit'>
+                        <h2 className='font-bold tracking-tight text-xl'>Mensaje</h2>
+                        <div
+                            className='w-full p-2 rounded-md text-white bg-gray-800'>
+                            <div className="flex max-w-sm bg-gray-800 py-2">
+                                <div className="wrap-anywhere">
+                                    <p className='text-white bg-green-900 rounded-md p-5'>{sendMesage()}</p>
+                                </div>
+                            </div>
+                            <hr className='border-black w-full' />
+                            <div className='flex gap-3 items-center justify-between bg-gray-900 w-full p-2 rounded-md'>
+                                <button
+                                    className='hover:bg-gray-800 rounded-md p-2 shadow-sm transition-all group hover:scale-105'
+                                    type='button'>
+                                    <PiPaperclipBold className='hover:bg-gray-800 text-gray-400 rounded-md shadow-sm transition-all group hover:scale-105' />
+                                </button>
+                                <label className='text-gray-400 items-center w-full flex hover:bg-gray-800 rounded-md p-2 transition-all group hover:scale-105'>
+                                    <input
+                                        className='cursor-pointer mr-2'
+                                        type='checkbox'
+                                        onChange={(e) => {
+                                            setImo(e.target.checked)
+                                        }}
+                                    />{'  Incluir mensaje opcional'}
+                                </label>
+                                <button
+                                    className='hover:bg-gray-800 rounded-md p-2 shadow-sm transition-all group hover:scale-105'
+                                    type='submit'>
+                                    <VscSend className='hover:bg-gray-800 text-gray-400 rounded-md shadow-sm transition-all group hover:scale-105' />
+                                </button>
+                            </div>
                             {imo && (
                                 <textarea
-                                    className="w-full p-2 border border-gray-600 rounded-md"
+                                    className='w-full text-gray-900 p-2 border border-gray-600 rounded-md'
                                     value={msjo}
                                     onChange={(e) => {
                                         setMsjo(e.target.value)
                                     }}
-                                    autoComplete="on"
-                                    autoCorrect="on"
+                                    autoComplete='on'
+                                    autoCorrect='on'
                                     onInput={adjustHeight}
                                 />
                             )}
                         </div>
-                        <button
-						    className='btn-primary hover:bg-cyan-600'
-						    type='submit'>Enviar mensaje
-					    </button>
-                    </form>
-                </>
+                        
+                    </div>
+                </form>
                 :
-                <p className='text-gray-600'>No tienes envíos de mensajes pendientes.</p>
+                <div className='flex gap-3 items-center justify-center h-full'>
+                    <p className='text-gray-600'>No tienes envíos de mensajes pendientes.</p>
+                </div>
             }
         </div>
     );
