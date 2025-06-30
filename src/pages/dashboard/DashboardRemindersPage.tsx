3f376a2064e674717c406167a01d02e5772b7d6e
Marcos19842024 <chable240608@gmail.com>
2025-06-29 23:24:59 -0600

    mac15

--- src/pages/dashboard/DashboardRemindersPage.tsx
+++ src/pages/dashboard/DashboardRemindersPage.tsx
@@ -24,28 +24,29 @@ export const DashboardRemindersPage = () => {
 
   return (
     <div className="relative h-full flex flex-col gap-1">
-      {showIb ?
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-bold">Envío de mensajes</h1>
-          <input
-            type="file"
-            id="inputfile"
-            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            onChange={handleFile}
-            className="file-input file-input-bordered w-full max-w-xs"
-            hidden
-          />
-          <label
-            htmlFor="inputfile"
-            className='bg-black text-white flex items-center self-end py-[6px] px-2 rounded-md text-sm gap-1 font-semibold hover:bg-cyan-600'>
-            <span><i className="fa fa-cloud-upload fa-2x"></i></span>
-            <p>Click To Upload List</p>
-          </label>
-        </div>
-      :
-        <div className="flex flex-col gap-1">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Envío de mensajes</h1>
+        {showIb ?
+          //poner aqui un boton de whatsapp para conectarse al servidor y traer los contactos
+          //pasar el componente de whatsapp y de servidor que estan en reminders aqui
           <div className="flex items-center justify-between">
-            <h1 className="text-2xl font-bold">Envío de mensajes</h1>
+            <input
+              type="file"
+              id="inputfile"
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+              onChange={handleFile}
+              className="file-input file-input-bordered w-full max-w-xs"
+              hidden
+            />
+            <label
+              htmlFor="inputfile"
+              className='bg-black text-white flex items-center self-end py-[6px] px-2 rounded-md text-sm gap-1 font-semibold hover:bg-cyan-600'>
+              <span><i className="fa fa-cloud-upload fa-2x"></i></span>
+              <p>Click To Upload List</p>
+            </label>
+          </div>
+        :
+          <div className="flex flex-col gap-1">
             {info && (
               <label>
                 <span>
@@ -61,9 +62,9 @@ export const DashboardRemindersPage = () => {
               </label>
             )}
           </div>
-          <Reminders clientes={clientes} />
-        </div>
-      }
+        }
+      </div>
+      <Reminders clientes={clientes} />
     </div>
   )
 }
